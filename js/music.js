f50fbbd0fd8a6ed24ba2c26183d52dedaba97363
Clay-andis <andi_escorpion_17@hotmail.com>
2023-08-23 00:58:00 -0500

    ultimo

--- js/music.js
+++ js/music.js
@@ -11,7 +11,7 @@ const $botonReproducir = document.querySelector("#btnReproducir"),
     $botonPausar = document.querySelector("#btnPausar"),
     $botonReiniciar = document.querySelector("#btnReiniciar");
 // El sonido que podemos reproducir o pausar
-const sonido = cargarSonido("https://github.com/Clay-andis/florandi/blob/d46fe149f9d30e8e04a613af490048bdfa6bd8dc/music/thousand.mp3");
+const sonido = cargarSonido("./music/thousand.mp3");
 $botonReproducir.onclick = () => {
     sonido.play();
 };
